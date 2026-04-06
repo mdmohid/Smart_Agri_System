4916bc50109f817dd0bebfab5581d0cd2c846a66
mdmohid <alammohid855@gmail.com>
2026-04-07 02:00:16 +0545

    ui layouts updates

--- smart_agri_system/settings.py
+++ smart_agri_system/settings.py
@@ -18,6 +18,10 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -121,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#media directory for image uploads
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- smart_agri_system/urls.py
+++ smart_agri_system/urls.py
@@ -17,9 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # path('advisory_system/', include('advisory_system.urls')), #crop recommendation
     path('', include('advisory_system.urls')),
 ]
+
+
+
+#for media disease images
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
